472352235c3dbca3098181b47087f537a4647819
Monica R <mlisareyes@gmail.com>
2019-10-07 03:01:52 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,7 +19,23 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
     doc.css(".social-icon-container a").each do |link|
-      
+      student = {}
+    doc.css(".social-icon-container a").each do |link|
+      url = link.attr("href")
+      if url.include?("twitter")
+        student[:twitter] = url
+      elsif url.include?("linkedin")
+        student[:linkedin] = url
+      elsif url.include?("github")
+        student[:github] = url
+      else
+        student[:blog] = url
+      end
+    end
+    student[:profile_quote] = doc.css(".profile-quote").text if doc.css(".profile-quote").text
+    student[:bio] = doc.css("p").text if doc.css("p").text
+    student
+
     end
     binding.pry
 
